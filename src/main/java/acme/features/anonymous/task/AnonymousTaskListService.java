b4215439743011db42a2625e46ef830a1e31e88e
mardiafer13 <mardiafer13@alum.us.es>
2021-03-31 11:23:43 +0200

    changes in task - anonymous level

---
+++ src/main/java/acme/features/anonymous/task/AnonymousTaskListService.java
@@ -0,0 +1,67 @@
+package acme.features.anonymous.task;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.tasks.Task;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Anonymous;
+import acme.framework.services.AbstractListService;
+
+
+@Service
+public class AnonymousTaskListService implements AbstractListService<Anonymous, Task> {
+
+	
+	// Internal state ---------------------------------------------------------
+
+		@Autowired
+		protected AnonymousTaskRepository repository;
+
+
+	// AbstractListService<Administrator, Shout> interface --------------
+
+		
+	@Override
+	public boolean authorise(final Request<Task> request) {
+		assert request != null;
+		
+		return true;
+	}
+
+	@Override
+	public void unbind(final Request<Task> request, final Task entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "author", "text", "moment");
+		
+	}
+
+	@Override
+	public Collection<Task> findMany(final Request<Task> request) {
+		final List<Task> res = new ArrayList<>();
+		final Collection<Task> tasks = this.repository.findMany();
+		final Date now = new Date();
+		for(final Task t: tasks) {
+			if(t.getPeriodFinal().before(now)) {
+				res.add(t);
+			}
+		}
+		Collections.sort(res, Comparator.comparing(x->x.getPeriodInitial()));
+//		for(final Task t: res) {
+//			System.out.println(t.getPeriodFinal());
+//		}
+		return res;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/anonymous/task/list.jsp
@@ -0,0 +1,13 @@
+<%@page language="java"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" tagdir="/WEB-INF/tags"%>
+
+<acme:list readonly="true">
+	<acme:list-column code="anonymous.task.list.label.title" path="title" width="20%"/>
+	<acme:list-column code="anonymous.task.list.label.periodInitial" path="periodInitial" width="20%"/>
+	<acme:list-column code="anonymous.task.list.label.periodFinal" path="periodFinal" width="20%"/>
+	<acme:list-column code="anonymous.task.list.label.description" path="description" width="20%"/>
+	<acme:list-column code="anonymous.task.list.label.link" path="link" width="20%"/>
+
+</acme:list>
